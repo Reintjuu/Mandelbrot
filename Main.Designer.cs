6d4d3ae99a37bd5335938c325b83f4a79de8c45a
Reinier de Vries <reiniervries@outlook.com>
2018-12-04 23:04:38 +0100

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Mandelbrot
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.canvas = new System.Windows.Forms.PictureBox();
+			this.centerXLabel = new System.Windows.Forms.Label();
+			this.centerXTextBox = new System.Windows.Forms.TextBox();
+			this.centerYTextBox = new System.Windows.Forms.TextBox();
+			this.centerYLabel = new System.Windows.Forms.Label();
+			this.maxLabel = new System.Windows.Forms.Label();
+			this.scaleLabel = new System.Windows.Forms.Label();
+			this.okButton = new System.Windows.Forms.Button();
+			this.maxInput = new System.Windows.Forms.NumericUpDown();
+			this.scaleTextBox = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.maxInput)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// canvas
+			// 
+			this.canvas.Location = new System.Drawing.Point(0, 66);
+			this.canvas.Name = "canvas";
+			this.canvas.Size = new System.Drawing.Size(400, 400);
+			this.canvas.TabIndex = 0;
+			this.canvas.TabStop = false;
+			this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseClick);
+			// 
+			// centerXLabel
+			// 
+			this.centerXLabel.AutoSize = true;
+			this.centerXLabel.Location = new System.Drawing.Point(12, 15);
+			this.centerXLabel.Name = "centerXLabel";
+			this.centerXLabel.Size = new System.Drawing.Size(51, 13);
+			this.centerXLabel.TabIndex = 1;
+			this.centerXLabel.Text = "Center X:";
+			// 
+			// centerXTextBox
+			// 
+			this.centerXTextBox.Location = new System.Drawing.Point(69, 12);
+			this.centerXTextBox.Name = "centerXTextBox";
+			this.centerXTextBox.Size = new System.Drawing.Size(100, 20);
+			this.centerXTextBox.TabIndex = 2;
+			this.centerXTextBox.Text = "0";
+			// 
+			// centerYTextBox
+			// 
+			this.centerYTextBox.Location = new System.Drawing.Point(69, 38);
+			this.centerYTextBox.Name = "centerYTextBox";
+			this.centerYTextBox.Size = new System.Drawing.Size(100, 20);
+			this.centerYTextBox.TabIndex = 4;
+			this.centerYTextBox.Text = "0";
+			// 
+			// centerYLabel
+			// 
+			this.centerYLabel.AutoSize = true;
+			this.centerYLabel.Location = new System.Drawing.Point(12, 41);
+			this.centerYLabel.Name = "centerYLabel";
+			this.centerYLabel.Size = new System.Drawing.Size(51, 13);
+			this.centerYLabel.TabIndex = 3;
+			this.centerYLabel.Text = "Center Y:";
+			// 
+			// maxLabel
+			// 
+			this.maxLabel.AutoSize = true;
+			this.maxLabel.Location = new System.Drawing.Point(186, 41);
+			this.maxLabel.Name = "maxLabel";
+			this.maxLabel.Size = new System.Drawing.Size(30, 13);
+			this.maxLabel.TabIndex = 7;
+			this.maxLabel.Text = "Max:";
+			// 
+			// scaleLabel
+			// 
+			this.scaleLabel.AutoSize = true;
+			this.scaleLabel.Location = new System.Drawing.Point(186, 15);
+			this.scaleLabel.Name = "scaleLabel";
+			this.scaleLabel.Size = new System.Drawing.Size(37, 13);
+			this.scaleLabel.TabIndex = 5;
+			this.scaleLabel.Text = "Scale:";
+			// 
+			// okButton
+			// 
+			this.okButton.Location = new System.Drawing.Point(290, 37);
+			this.okButton.Name = "okButton";
+			this.okButton.Size = new System.Drawing.Size(39, 23);
+			this.okButton.TabIndex = 9;
+			this.okButton.Text = "OK";
+			this.okButton.UseVisualStyleBackColor = true;
+			this.okButton.Click += new System.EventHandler(this.OkButton_Click);
+			// 
+			// maxInput
+			// 
+			this.maxInput.Location = new System.Drawing.Point(229, 38);
+			this.maxInput.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+			this.maxInput.Name = "maxInput";
+			this.maxInput.Size = new System.Drawing.Size(55, 20);
+			this.maxInput.TabIndex = 8;
+			this.maxInput.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+			// 
+			// scaleTextBox
+			// 
+			this.scaleTextBox.Location = new System.Drawing.Point(229, 12);
+			this.scaleTextBox.Name = "scaleTextBox";
+			this.scaleTextBox.Size = new System.Drawing.Size(100, 20);
+			this.scaleTextBox.TabIndex = 6;
+			this.scaleTextBox.Text = "1";
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(401, 467);
+			this.Controls.Add(this.maxInput);
+			this.Controls.Add(this.okButton);
+			this.Controls.Add(this.maxLabel);
+			this.Controls.Add(this.scaleTextBox);
+			this.Controls.Add(this.scaleLabel);
+			this.Controls.Add(this.centerYTextBox);
+			this.Controls.Add(this.centerYLabel);
+			this.Controls.Add(this.centerXTextBox);
+			this.Controls.Add(this.centerXLabel);
+			this.Controls.Add(this.canvas);
+			this.Name = "Main";
+			this.Text = "Mandelbrot";
+			this.ResizeEnd += new System.EventHandler(this.Main_ResizeEnd);
+			this.Resize += new System.EventHandler(this.Main_Resize);
+			((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.maxInput)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox canvas;
+		private System.Windows.Forms.Label centerXLabel;
+		private System.Windows.Forms.TextBox centerXTextBox;
+		private System.Windows.Forms.TextBox centerYTextBox;
+		private System.Windows.Forms.Label centerYLabel;
+		private System.Windows.Forms.Label maxLabel;
+		private System.Windows.Forms.Label scaleLabel;
+		private System.Windows.Forms.Button okButton;
+		private System.Windows.Forms.NumericUpDown maxInput;
+		private System.Windows.Forms.TextBox scaleTextBox;
+	}
+}
+
---
+++ Main.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace Mandelbrot
+{
+	public partial class Main : Form
+	{
+		private const double MIN = -2.5;
+		private const double MAX = 2.5;
+
+		private double centerX;
+		private double centerY;
+		private double scale;
+		private int maxIterations;
+
+		// Initially set to minimized to trigger first maximize button click properly.
+		private FormWindowState lastWindowState = FormWindowState.Minimized;
+		private Timer animationTimer;
+
+		public Main()
+		{
+			InitializeComponent();
+
+			ReadUIValues();
+			// Properly size the PictureBox and draw the first Mandelbrot.
+			ResizeMandelbrot(this);
+
+			animationTimer = new Timer();
+			animationTimer.Tick += AnimationTimer_Tick;
+			animationTimer.Interval = 1;
+			StartAnimation();
+		}
+
+		/// <summary>
+		/// Making sure the maximize button works as well, to redraw the Mandelbrot.
+		/// </summary>
+		private void Main_Resize(object sender, EventArgs e)
+		{
+			if (WindowState != lastWindowState)
+			{
+				lastWindowState = WindowState;
+
+				if (WindowState == FormWindowState.Maximized || WindowState == FormWindowState.Normal)
+				{
+					ResizeMandelbrot(sender);
+				}
+			}
+		}
+
+		private void Main_ResizeEnd(object sender, EventArgs e)
+		{
+			ResizeMandelbrot(sender);
+		}
+
+		private void ResizeMandelbrot(object sender)
+		{
+			Control control = (Control) sender;
+			// Calculate the canvas' size to be the size of the Form - the height of the control panel (where the input fields are).
+			canvas.Size = new Size(
+				control.ClientRectangle.Size.Width,
+				control.ClientRectangle.Size.Height - canvas.Location.Y);
+
+			DrawMandelbrot();
+		}
+
+		private void OkButton_Click(object sender, EventArgs e)
+		{
+			ReadUIValues();
+			DrawMandelbrot();
+		}
+
+		private void ReadUIValues()
+		{
+			double oldValue = centerX;
+			centerXTextBox.Text = centerXTextBox.Text.Replace(',', '.');
+			if (!double.TryParse(centerXTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out centerX))
+			{
+				centerX = oldValue;
+			}
+
+			oldValue = centerY;
+			centerYTextBox.Text = centerYTextBox.Text.Replace(',', '.');
+			if (!double.TryParse(centerYTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out centerY))
+			{
+				centerY = oldValue;
+			}
+
+			oldValue = scale;
+			scaleTextBox.Text = scaleTextBox.Text.Replace(',', '.');
+			if (!double.TryParse(scaleTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out scale))
+			{
+				scale = oldValue;
+			}
+
+			maxIterations = (int) maxInput.Value;
+
+			WriteValuesToUI();
+		}
+
+		private void WriteValuesToUI()
+		{
+			centerXTextBox.Text = centerX.ToString(CultureInfo.InvariantCulture);
+			centerYTextBox.Text = centerY.ToString(CultureInfo.InvariantCulture);
+			scaleTextBox.Text = scale.ToString(CultureInfo.InvariantCulture);
+		}
+
+		private void DrawMandelbrot()
+		{
+			int height = canvas.ClientSize.Height;
+			int width = canvas.ClientSize.Width;
+			var bitmap = new Bitmap(width, height);
+
+			// Calculate offsets, to always center the Mandelbrot.
+			int sizeMin = Math.Min(width, height);
+			double offsetX = (width - sizeMin) / 2d;
+			double offsetY = (height - sizeMin) / 2d;
+
+			for (int y = 0; y < height; y++)
+			{
+				for (int x = 0; x < width; x++)
+				{
+					double scaledX = MapRange(x, offsetX, sizeMin + offsetX, MIN, MAX) * scale + centerX;
+					double scaledY = MapRange(y, offsetY, sizeMin + offsetY, MIN, MAX) * scale + centerY;
+
+					int mandelbrot = CalculateMandelbrot(scaledX, scaledY, maxIterations);
+					Color color = mandelbrot == maxIterations || mandelbrot % 2 != 0
+						? Color.Black
+						: Color.FromArgb(mandelbrot % 128 * 2, mandelbrot % 32 * 7, mandelbrot % 16 * 14);
+					bitmap.SetPixel(x, y, color);
+				}
+			}
+
+			// Clear the previous bitmap.
+			canvas.Image?.Dispose();
+			canvas.Image = bitmap;
+		}
+
+		private int CalculateMandelbrot(double x, double y, int iterations)
+		{
+			double a = 0;
+			double b = 0;
+
+			int i = 0;
+			while (a * a + b * b < 4 && i < iterations)
+			{
+				double aTemp = a * a - b * b + x;
+				b = 2 * a * b + y;
+				a = aTemp;
+
+				i++;
+			}
+
+			return i;
+		}
+
+		public static double MapRange(double value, double fromMin, double fromMax, double toMin, double toMax)
+		{
+			return toMin + (value - fromMin) * (toMax - toMin) / (fromMax - fromMin);
+		}
+
+		private void Canvas_MouseClick(object sender, MouseEventArgs e)
+		{
+			// Zoom out on right click and zoom in on any other mouse button.
+			if (e.Button == MouseButtons.Right)
+			{
+				scale *= 2;
+			}
+			else
+			{
+				scale /= 2;
+			}
+
+			centerX += MapRange(e.X, 0, canvas.ClientSize.Width, MIN, MAX) * scale;
+			centerY += MapRange(e.Y, 0, canvas.ClientSize.Height, MIN, MAX) * scale;
+
+			WriteValuesToUI();
+			DrawMandelbrot();
+		}
+
+		private void StartAnimation()
+		{
+			centerX = -1.12717668726378;
+			centerY = -0.26968634665177;
+			centerX = 0.349372201726918;
+			centerY = 0.512074673562276;
+			animationTimer.Start();
+		}
+
+		private void AnimationTimer_Tick(object sender, EventArgs e)
+		{
+			scale /= 1.01;
+			WriteValuesToUI();
+			DrawMandelbrot();
+		}
+	}
+}
